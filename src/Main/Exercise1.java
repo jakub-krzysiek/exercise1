64f7c9dfc6ccf296131e8ad2c8b0009e2e30bf39
jakub-krzysiek <kuba.krzysiek96@gmail.com>
2023-02-27 22:20:02 +0100

    Added first exercise

--- src/Main/Exercise1.java
+++ src/Main/Exercise1.java
@@ -3,6 +3,9 @@ package Main;
 public class Exercise1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int x = 5;
+        int y = 10;
+        int z = x+y;
+        System.out.print(z);
     }
 }
